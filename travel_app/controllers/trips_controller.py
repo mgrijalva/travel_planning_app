145115ca202eb2efbd49925ad3214da78ded6952
iampruven <59456028+iampruven@users.noreply.github.com>
2025-11-10 13:04:06 -0800

    1-users-are-able-to-add-new-trips (#9)
    * Implement trip creation functionality and update trip listing
    
    * remove unused import

--- travel_app/controllers/trips_controller.py
+++ travel_app/controllers/trips_controller.py
@@ -1,10 +1,32 @@
-from flask import render_template
+from flask import render_template, redirect, request
 from ..db import get_db
-from ..repositories.trips_repository import TripsRepository
+from psycopg2.extras import DictCursor
 
 
 def list():
     conn = get_db()
-    tr = TripsRepository(conn)
-    trips = tr.all()
-    return render_template("trips/index.html", trips=trips)
+    with conn.cursor(cursor_factory=DictCursor) as cursor:
+        cursor.execute(
+            """
+            select * from trips;
+        """
+        )
+        data = cursor.fetchall()
+    return render_template("trips/index.html", trips=data)
+
+
+def create_trip():
+    title = request.form["title"]
+    start_date = request.form["start-date"]
+    end_date = request.form["end-date"]
+
+    conn = get_db()
+    with conn.cursor() as cursor:
+        cursor.execute(
+            """
+            insert into trips (title,start_date,end_date) values(%s, %s, %s)
+                       """,
+            (title, start_date, end_date),
+        )
+        conn.commit()
+    return redirect("/")
