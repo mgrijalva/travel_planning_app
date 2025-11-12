3b99c018f9a9cd732df47f62cb5225aaf0c0e2d4
iampruven <iampruven@yahoo.com>
2025-11-12 15:17:33 -0800

    Implement trip details view and event creation functionality

--- travel_app/controllers/trips_controller.py
+++ travel_app/controllers/trips_controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, url_for
 from ..db import get_db
 from psycopg2.extras import DictCursor
 
@@ -30,3 +30,33 @@ def create_trip():
         )
         conn.commit()
     return redirect("/")
+
+
+def trip_details(trip_id):
+    conn = get_db()
+    if request.method == "GET":
+        with conn.cursor(cursor_factory=DictCursor) as cursor:
+            cursor.execute("select * from trips where id = %s", (trip_id,))
+            trip = cursor.fetchone()
+
+            cursor.execute(
+                "select * from events where trip_id = %s order by event_time",
+                (trip_id,),
+            )
+            events = cursor.fetchall()
+            return render_template(
+                "trips/trip.html", events=events, trip=trip, trip_id=trip_id
+            )
+    else:
+        event_name = request.form["event-name"]
+        event_time = request.form["event-time"]
+        address = request.form["address"]
+
+        conn = get_db()
+        with conn.cursor() as cursor:
+            cursor.execute(
+                "insert into events (trip_id, event_name, event_time, address) values( %s, %s, %s, %s)",
+                (trip_id, event_name, event_time, address),
+            )
+            conn.commit()
+        return redirect(url_for("trip_details", trip_id=trip_id))
