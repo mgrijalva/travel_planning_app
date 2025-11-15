e4db86c4a5f3254007843c18c9ab424c507f234d
Michael Grijalva <michaelgrij@gmail.com>
2025-11-15 12:11:31 -0800

    Revert "commit a bad migration to test it"
    This reverts commit 9d6657a9e28b881ed3d8b640fb61b8898a45847a.

--- migrations/V4__durp.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE haha (
-    id SERIAL PRIMARY KEY,
-    trip_id INddddT REFERENCES trips(id) ON DELETE CASCADE,
-    event_name TEXT NOT NULL,
-    event_time TIMESTAMP,
-    address TEXT NOT NULL
-);
